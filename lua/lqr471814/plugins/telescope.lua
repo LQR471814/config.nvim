cf5a265ce65575b55f25d941bdee4aac1d11043e
LQR471814 <42160088+LQR471814@users.noreply.github.com>
2025-08-07 13:54:18 -0700

    add telescope shortcuts

--- lua/lqr471814/plugins/telescope.lua
+++ lua/lqr471814/plugins/telescope.lua
@@ -34,6 +34,35 @@ return {
             vim.keymap.set("n", "<leader>pe", "<CMD>Telescope diagnostics<CR>")
             vim.keymap.set("n", "<leader>pc", "<CMD>Telescope commands<CR>")
             vim.keymap.set("n", "<leader>ph", "<CMD>Telescope help_tags<CR>")
+
+            local oil = require("oil")
+            vim.keymap.set("n", "<leader>pd", function()
+                local dir = oil.get_current_dir()
+                if not dir then
+                    oil.open()
+                end
+                dir = oil.get_current_dir()
+
+                builtin.find_files({
+                    prompt_title = "Files from the current directory",
+                    cwd = dir,
+                })
+            end, { desc = "Find files from the current directory" })
+
+            vim.keymap.set("n", "<leader>pj", function()
+                builtin.find_files({
+                    prompt_title = "Projects",
+                    cwd = "./Projects",
+                    find_command = { 'fd', '--type', 'd', '--max-depth', '2', '--exclude', 'Archive' },
+                })
+            end, { desc = "Find projects" })
+            vim.keymap.set("n", "<leader>pi", function()
+                builtin.find_files({
+                    prompt_title = "Information",
+                    cwd = "./Information",
+                    find_command = { 'fd', '--type', 'd', '--max-depth', '1' },
+                })
+            end, { desc = "Find information" })
         end
     }
 }
