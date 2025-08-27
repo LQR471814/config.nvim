ffb55c1c85b4afb9cfac532526ea3eb8a223624f
LQR471814 <42160088+LQR471814@users.noreply.github.com>
2025-08-27 08:21:38 -0700

    add local config

--- lua/lqr471814/layers/extras.lua
+++ lua/lqr471814/layers/extras.lua
@@ -18,4 +18,18 @@ return {
 		"lervag/vimtex",
 		event = "VeryLazy",
 	},
+	-- local config
+	{
+		"klen/nvim-config-local",
+		config = function()
+			require("config-local").setup({
+				config_files = { ".nvim.lua" },
+				hashfile = vim.fn.stdpath("data") .. "/config-local",
+				autocommands_create = true,
+				commands_create = true,
+				silent = false,
+				lookup_parents = false,
+			})
+		end
+	}
 }
