ebc16280af2166faecec0a1abe046ed37a248be7
LQR471814 <42160088+LQR471814@users.noreply.github.com>
2025-09-26 14:51:27 -0700

    feat: improve vim matchup integration

--- lua/lqr471814/layers/core.lua
+++ lua/lqr471814/layers/core.lua
@@ -64,10 +64,13 @@ return {
 	{
 		"andymass/vim-matchup",
 		event = "VeryLazy",
+		init = function()
+			vim.g.matchup_matchparen_offscreen = { method = "popup" }
+		end,
 		---@type matchup.Config
 		opts = {
 			treesitter = {
-				stopline = 500,
+				stopline = 1500,
 			}
 		}
 	},
