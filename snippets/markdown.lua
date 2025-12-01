e83e3c2c806b38df67f6e9e012ff3498b8a9e1f1
LQR471814 <42160088+LQR471814@users.noreply.github.com>
2025-11-30 17:11:20 -0800

    feat: add explicit wordtrig

--- snippets/markdown.lua
+++ snippets/markdown.lua
@@ -4,16 +4,16 @@ local i = ls.insert_node
 local fmta = require("luasnip.extras.fmt").fmta
 
 return {
-    s({ trig = "chk" }, fmta("[ ] ", { })),
-    s({ trig = "mk", snippetType = "autosnippet" }, fmta("$<>$", { i(1) })),
-    s({ trig = "dm", snippetType = "autosnippet" }, fmta(
+    s({ trig = "chk" }, fmta("[ ] ", {})),
+    s({ trig = "mk", snippetType = "autosnippet", wordTrig = true }, fmta("$<>$", { i(1) })),
+    s({ trig = "dm", snippetType = "autosnippet", wordTrig = true }, fmta(
         [[
         $$
         <>
         $$
         ]],
-    { i(1) })),
-    s({ trig = "adm", snippetType = "autosnippet" }, fmta(
+        { i(1) })),
+    s({ trig = "adm", snippetType = "autosnippet", wordTrig = true }, fmta(
         [[
         $$
         \begin{aligned}
