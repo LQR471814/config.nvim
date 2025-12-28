f2706f4df0cbbd939b50e6c45869f34220e9c869
LQR471814 <42160088+LQR471814@users.noreply.github.com>
2025-12-28 09:43:52 -0800

    feat: use fzf-native

--- lua/lqr471814/plugins/telescope.lua
+++ lua/lqr471814/plugins/telescope.lua
@@ -12,25 +12,35 @@ return {
             "nvim-lua/plenary.nvim",
             "nvim-telescope/telescope-fzf-native.nvim",
         },
-        opts = {
-            defaults = {
-                path_display = { "truncate" },
-                layout_strategy = "flex",
-                layout_config = {
-                    flex = {
-                        flip_columns = 120,
-                        flip_lines   = 20,
-                    },
-                    horizontal = {
-                        preview_width  = 0.4,
-                        preview_cutoff = 40,
-                    },
-                    vertical = {
-                        preview_height = 0.5,
-                        preview_cutoff = 10,
+        config = function()
+            local telescope = require("telescope")
+            telescope.setup({
+                defaults = {
+                    path_display = { "truncate" },
+                    layout_strategy = "flex",
+                    layout_config = {
+                        flex = {
+                            flip_columns = 120,
+                            flip_lines   = 20,
+                        },
+                        horizontal = {
+                            preview_width  = 0.4,
+                            preview_cutoff = 40,
+                        },
+                        vertical = {
+                            preview_height = 0.5,
+                            preview_cutoff = 10,
+                        },
                     },
                 },
-            },
-        },
+                extensions = {
+                    fuzzy = true,
+                    override_generic_sorter = true,
+                    override_file_sorter = true,
+                    case_mode = "smart_case",
+                }
+            })
+            telescope.load_extension("fzf")
+        end
     }
 }
