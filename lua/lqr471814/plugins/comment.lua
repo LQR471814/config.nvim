98cf486429f9588e4baef8f5b9ee364a5466f4cc
LQR471814 <42160088+LQR471814@users.noreply.github.com>
2026-01-11 11:18:32 -0800

    feat: add ts-comment.nvim

--- lua/lqr471814/plugins/comment.lua
+++ lua/lqr471814/plugins/comment.lua
@@ -1,10 +1,8 @@
 return {
-    "numToStr/Comment.nvim",
-    event = "VeryLazy",
-    dependencies = { "JoosepAlviste/nvim-ts-context-commentstring" },
-    config = function()
-        require("Comment").setup({
-            pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
-        })
-    end
+    {
+        "folke/ts-comments.nvim",
+        opts = {},
+        event = "VeryLazy",
+        enabled = vim.fn.has("nvim-0.10.0") == 1,
+    }
 }
