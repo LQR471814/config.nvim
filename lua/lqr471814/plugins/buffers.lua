7327a4ec46ee000ff67fe16fb08f901b4cea40b2
LQR471814 <42160088+LQR471814@users.noreply.github.com>
2026-04-11 16:32:57 -0700

    feat: remove scrub

--- lua/lqr471814/plugins/buffers.lua
+++ lua/lqr471814/plugins/buffers.lua
@@ -4,12 +4,4 @@ return {
 		'axkirillov/hbac.nvim',
 		config = true,
 	},
-	{
-		"n3tw0rth/keeper.nvim",
-		config = function()
-			require("scrub").setup()
-			local keymap = require("lqr471814.lib.keymap")
-			keymap.map("n", "<leader>pb", "<cmd>Scrub<cr>", "Open scrub.")
-		end
-	},
 }
