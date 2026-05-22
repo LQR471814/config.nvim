c2383ee3c8a89731d81e9f7d0d35cf5aa81e5ee7
LQR471814 <42160088+LQR471814@users.noreply.github.com>
2026-05-22 10:29:39 -0700

    feat(surround): better surround plugin

--- lua/lqr471814/plugins/surround.lua
+++ lua/lqr471814/plugins/surround.lua
@@ -1,28 +1,46 @@
 return {
-    "kylechui/nvim-surround",
+    "nvim-mini/mini.surround",
     event = "VeryLazy",
+    version = false,
     config = function()
-        require("nvim-surround").setup({
-            -- disables the whitespace in front and back of the thing
-            surrounds = {
-                ["<"] = false,
-                ["("] = false,
-                ["["] = false,
-                ["{"] = false,
-            },
-            aliases = {
-                ["("] = ")",
-                ["["] = "]",
-                ["{"] = "}",
-            },
-        })
-    end
-}
+        local keymap = require("lqr471814.lib.keymap")
 
--- quick reference
--- yss<new> - surround line with <new>
--- ysiw<new> - surround word with <new>
--- ysa<thing><new> - surround <thing> with <new>
--- (in visual mode) S<new> - surround selection with <new>
--- (in visual line mode) gS<new> - surround each selection with <new>
--- cs/ds<target><new> - replace <target> with <new>
+        local surround = require("mini.surround")
+        surround.setup({
+            -- Preserve current behavior:
+            -- use no-space pairs for opening bracket keys too.
+            custom_surroundings = {
+                ["("] = { output = { left = "(", right = ")" } },
+                ["["] = { output = { left = "[", right = "]" } },
+                ["{"] = { output = { left = "{", right = "}" } },
+                ["<"] = { output = { left = "<", right = ">" } },
+            },
+
+            mappings = {
+                add = "ys",
+                delete = "ds",
+                replace = "cs",
+
+                find = "",
+                find_left = "",
+                highlight = "",
+
+                suffix_last = "",
+                suffix_next = "",
+            },
+
+            -- Avoid linewise/blockwise surround changing layout/indent.
+            respect_selection_type = false,
+
+            -- Similar to classic surround: operate on covering pair only.
+            search_method = "cover",
+        })
+
+        -- yss<new> => surround whole line
+        keymap.overwrite_map("n", "yss", "ys_", "Surround the whole line.")
+
+        -- Visual S<new> => surround selection
+        keymap.unmap("x", "ys")
+        keymap.map("x", "S", [[:<C-u>lua MiniSurround.add('visual')<CR>]], "Surround the visual selection.")
+    end,
+}
