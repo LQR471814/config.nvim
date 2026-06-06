126dd586d981123fe5a06657e487fdc87dfe8a41
LQR471814 <42160088+LQR471814@users.noreply.github.com>
2026-06-05 20:10:46 -0700

    fix(treesitter): add treesitter-parser-registry

--- lua/lqr471814/plugins/treesitter.lua
+++ lua/lqr471814/plugins/treesitter.lua
@@ -1,7 +1,8 @@
 return {
     "nvim-treesitter/nvim-treesitter",
+    dependencies = { 'neovim-treesitter/treesitter-parser-registry' },
     lazy = false,
-    build = ":TSUpdate",
+    build = ':TSUpdate',
     config = function()
         require("nvim-treesitter.configs").setup {
             ensure_installed = {},
