7abf4362a636a09456797357bd2e5f503adab74a
LQR471814 <42160088+LQR471814@users.noreply.github.com>
2026-08-10 07:21:41 -0700

    feat(theme): add dayfox

--- lua/lqr471814/plugins/theme.lua
+++ lua/lqr471814/plugins/theme.lua
@@ -1,6 +1,9 @@
 return {
-	"sho-87/kanagawa-paper.nvim",
-	config = function()
-		vim.cmd("colorscheme kanagawa-paper")
-	end
+	{
+		"sho-87/kanagawa-paper.nvim",
+		config = function()
+			vim.cmd("colorscheme kanagawa-paper")
+		end
+	},
+	{ "EdenEast/nightfox.nvim" }
 }
