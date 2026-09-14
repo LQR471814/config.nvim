03a6eae5012c03f618373d541a4335f8dbd5c36a
LQR471814 <42160088+LQR471814@users.noreply.github.com>
2026-09-14 07:12:51 -0700

    feat(diffview): remove keymap

--- lua/lqr471814/plugins/diffview.lua
+++ lua/lqr471814/plugins/diffview.lua
@@ -1,19 +1,5 @@
 return {
 	"sindrets/diffview.nvim",
 	cmd = { "DiffviewOpen", "DiffviewClose" },
-	config = function()
-		require("diffview").setup({})
-
-		local keymap = require("lqr471814.lib.keymap")
-
-		local open = false
-		keymap.map("n", "<leader>d", function()
-			if not open then
-				vim.cmd("DiffviewOpen")
-			else
-				vim.cmd("DiffviewClose")
-			end
-			open = not open
-		end, "Toggle diffview window.")
-	end
+	opts = {},
 }
