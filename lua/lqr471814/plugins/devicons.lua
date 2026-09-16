c6d63c11446713298e82389a9ec9f99ab48e97f6
LQR471814 <42160088+LQR471814@users.noreply.github.com>
2026-09-16 16:28:48 -0700

    fix(devicons): mini icons

--- lua/lqr471814/plugins/devicons.lua
+++ lua/lqr471814/plugins/devicons.lua
@@ -1,14 +1,19 @@
 return {
-	"nvim-tree/nvim-web-devicons",
-	config = function()
-		require("nvim-web-devicons").setup({
-			override_by_extension = {
-				tla = {
-					icon = "󰘧",
-					color = "#4B8BBE",
-					name = "TLA",
+	{
+		"echasnovski/mini.icons",
+		opts = {}
+	},
+	{
+		"nvim-tree/nvim-web-devicons",
+		config = function()
+			require("nvim-web-devicons").setup({
+				override_by_extension = {
+					tla = {
+						icon = "󰘧",
+						color = "#4B8BBE",
+						name = "TLA",
+					},
 				},
-			},
-		})
-	end
-}
+			})
+		end
+	} }
--- lua/lqr471814/plugins/oil.lua
+++ lua/lqr471814/plugins/oil.lua
@@ -1,64 +1,66 @@
 return {
-	"stevearc/oil.nvim",
-	dependencies = {
-		"echasnovski/mini.icons",
-		"nvim-tree/nvim-web-devicons",
-		"folke/snacks.nvim"
-	},
-	config = function()
-		require("oil").setup({
-			watch_for_changes = true,
-			skip_confirm_for_simple_edits = true,
-			lsp_file_methods = {
-				enabled = true,
-				timeout_ms = 1000,
-				autosave_changes = false,
-			},
-			view_options = {
-				show_hidden = true,
-				is_always_hidden = function(name, bufnr)
-					return name:match("_md_ltex%.json$")
-				end
-			}
-		})
+	{
+		"stevearc/oil.nvim",
+		dependencies = {
+			"echasnovski/mini.icons",
+			"nvim-tree/nvim-web-devicons",
+			"folke/snacks.nvim"
+		},
+		config = function()
+			require("oil").setup({
+				watch_for_changes = true,
+				skip_confirm_for_simple_edits = true,
+				lsp_file_methods = {
+					enabled = true,
+					timeout_ms = 1000,
+					autosave_changes = false,
+				},
+				view_options = {
+					show_hidden = true,
+					is_always_hidden = function(name, bufnr)
+						return name:match("_md_ltex%.json$")
+					end
+				}
+			})
 
-		-- automatically delete buffer when corresponding file on filesystem is deleted
-		vim.api.nvim_create_autocmd("User", {
-			pattern = "OilActionsPost",
-			callback = function(event)
-				if event.data.err then
-					return
-				end
-
-				for _, action in ipairs(event.data.actions) do
-					if action.entry_type ~= "file" then
-						goto continue
+			-- automatically delete buffer when corresponding file on filesystem is deleted
+			vim.api.nvim_create_autocmd("User", {
+				pattern = "OilActionsPost",
+				callback = function(event)
+					if event.data.err then
+						return
 					end
 
-					if action.type == "delete" then
-						local path = action.url:match("^.*://(.*)$")
-						Snacks.bufdelete({ file = path, wipe = true })
-					elseif action.type == "move" then
-						local path = action.src_url:match("^.*://(.*)$")
-						Snacks.bufdelete({ file = path, wipe = true })
+					for _, action in ipairs(event.data.actions) do
+						if action.entry_type ~= "file" then
+							goto continue
+						end
+
+						if action.type == "delete" then
+							local path = action.url:match("^.*://(.*)$")
+							Snacks.bufdelete({ file = path, wipe = true })
+						elseif action.type == "move" then
+							local path = action.src_url:match("^.*://(.*)$")
+							Snacks.bufdelete({ file = path, wipe = true })
+						end
+
+						::continue::
 					end
+				end,
+			})
 
-					::continue::
-				end
-			end,
-		})
-
-		-- notify snacks-rename of file rename
-		vim.api.nvim_create_autocmd("User", {
-			pattern = "OilActionsPost",
-			callback = function(event)
-				if #event.data.actions == 0 then
-					return
-				end
-				if event.data.actions[1].type == "move" then
-					Snacks.rename.on_rename_file(event.data.actions[1].src_url, event.data.actions[1].dest_url)
-				end
-			end,
-		})
-	end
+			-- notify snacks-rename of file rename
+			vim.api.nvim_create_autocmd("User", {
+				pattern = "OilActionsPost",
+				callback = function(event)
+					if #event.data.actions == 0 then
+						return
+					end
+					if event.data.actions[1].type == "move" then
+						Snacks.rename.on_rename_file(event.data.actions[1].src_url, event.data.actions[1].dest_url)
+					end
+				end,
+			})
+		end
+	}
 }
